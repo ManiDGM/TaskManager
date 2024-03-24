ce6e8b239849652aecfe0a1850316264a5e3c589
ManiDGM <manidgm1383@gmail.com>
2024-03-24 16:28:51 +0330

    project updated ManiDGM

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,4 +1,5 @@
 from controller.profile_controller import ProfileController
+from controller.task_controller import TaskController
 
 from controller.exception.no_content_error import NoContentError
 from controller.exception.access_denied_error import AccessDeniedError
---
+++ test/test_controller.py
@@ -0,0 +1,8 @@
+from controller import *
+
+#print(ProfileController.save("test", "test", "test@gmail.com", "t123"))
+#print(ProfileController.edit(1, "test2", "test2", "test@gmail.com", "test2"))
+#print(ProfileController.remove(2))
+
+#print(ProfileController.find_by_email("test@gmail.com"))
+#print(ProfileController.find_by_id(3))
